e53eac27c7014633f362e34f1a134b56f8a28551
root <root@ip-172-31-0-245.ap-south-1.compute.internal>
2023-11-02 02:50:24 +0000

    2nd commit

--- sbi.java
+++ sbi.java
@@ -4,7 +4,7 @@ class Animal {
 
 // use of extends keyword
 // to perform inheritance
-class Dog extends Animal {
+class Dog extends
 
   // methods and fields of Animal
   // methods and fields of Dog
